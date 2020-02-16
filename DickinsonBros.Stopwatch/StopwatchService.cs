f7899c92e9b4083d6ec52af2c643ce22126f9f1d
Mark Dickinson <marksamdickinson@gmail.com>
2020-02-16 14:26:44 -0700

    Add project files.

---
+++ DickinsonBros.Stopwatch/StopwatchService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics;
+using DickinsonBros.Stopwatch.Abstractions;
+
+namespace DickinsonBros.Stopwatch
+{
+    [ExcludeFromCodeCoverage]
+    public class StopwatchService : IStopwatchService
+    {
+        private readonly System.Diagnostics.Stopwatch _stopwatch;
+
+        public StopwatchService()
+        {
+            _stopwatch = new System.Diagnostics.Stopwatch();
+        }
+
+        public long ElapsedMilliseconds 
+        {
+            get => _stopwatch.ElapsedMilliseconds;
+        }
+
+        public TimeSpan Elapsed
+        {
+            get => _stopwatch.Elapsed;
+        }
+
+        public void Start()
+        {
+            _stopwatch.Start();
+        }
+
+        public void Stop()
+        {
+            _stopwatch.Stop();
+        }
+
+        public void Reset()
+        {
+            _stopwatch.Reset();
+        }
+    }
+}
